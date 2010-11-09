fa06c7dcb225457ff233c4efefbc597a26e9f082
Michał Zabłocki <michal.zablocki@hotmail.com>
2010-11-09 16:32:43 +0100

    fix for slugged_model.rb #2

--- lib/friendly_id/active_record_adapter/slugged_model.rb
+++ lib/friendly_id/active_record_adapter/slugged_model.rb
@@ -15,11 +15,12 @@ module FriendlyId
           extend FriendlyId::ActiveRecordAdapter::Finders unless FriendlyId.on_ar3?
           if friendly_id_config.slug_method == true
             define_method("#{friendly_id_config.method}=") do |*args|
-             #p 'setter'
-             super
-             build_a_slug # if args[0] == friendly_id_config.method.to_s
-           end
-         end
+              #p 'setter'
+              super
+              build_a_slug # if args[0] == friendly_id_config.method.to_s
+            end
+          end
+        end
       end
 
       include FriendlyId::Slugged::Model
